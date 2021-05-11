cbce84e61ed1befc342adc8409f498e42856e034
Cosmin Popovici <hellocosmin@gmail.com>
2021-05-11 16:45:51 +0300

    feat: extract version number to component

---
+++ src/components/LatestReleaseNumber.vue
@@ -0,0 +1,28 @@
+<template>
+  <span v-html="version" />
+</template>
+
+<script>
+export default {
+  name: 'LatestReleaseNumber',
+  computed: {
+    version() {
+      return this.$static.releases.edges[0].node.tag.replace('v', '')
+    }
+  }
+}
+</script>
+
+
+<static-query>
+query {
+  releases: allReleases {
+    edges {
+      node {
+        tag
+        url
+      }
+    }
+  }
+}
+</static-query>
